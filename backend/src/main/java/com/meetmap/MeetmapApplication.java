6bab73ef94064601e20b8a88f1835ff401cef59f
Alfiya Sheikh <alfiyasheikh0104@gmail.com>
2026-04-19 16:57:35 +0530

    remove DB dependency

--- backend/src/main/java/com/meetmap/MeetmapApplication.java
+++ backend/src/main/java/com/meetmap/MeetmapApplication.java
@@ -2,12 +2,15 @@ package com.meetmap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 public class MeetmapApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MeetmapApplication.class, args);
     }
-
 }
